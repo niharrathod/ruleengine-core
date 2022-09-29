bdc4eba1410b852b70282c6cb03abda3b0dc66eb
Nihar Rathod <nihar.b.rathod@gmail.com>
2022-09-29 19:38:43 +0530

    Initial benchmarking setup

---
+++ benchmark/ruleengine_test.go
@@ -0,0 +1,52 @@
+package benchmark
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	ruleenginecore "github.com/niharrathod/ruleengine-core"
+)
+
+var singleRuleEngine ruleenginecore.RuleEngine
+
+func init() {
+	wd, _ := os.Getwd()
+	valBytes, _ := os.ReadFile(wd + "/SingleRule.json")
+	var engineConfig ruleenginecore.RuleEngineConfig
+	err := json.Unmarshal(valBytes, &engineConfig)
+
+	if err != nil {
+		fmt.Println("json unmarshal failed : ", err.Error())
+		panic("No")
+	}
+
+	engine, reErr := ruleenginecore.New(&engineConfig)
+
+	if reErr != nil {
+		fmt.Println("json unmarshal failed : ", reErr.Error())
+		panic("No")
+	}
+
+	singleRuleEngine = engine
+}
+
+var singleRuleInputTable = []ruleenginecore.Input{
+	{
+		"TotalAmount":    "25000",
+		"IsHotelBooking": "true",
+		"PaxCount":       "10",
+	},
+}
+
+func BenchmarkRuleEngineComplete(b *testing.B) {
+	for i, input := range singleRuleInputTable {
+		b.Run(fmt.Sprintf("SingleRule_input_%d", i), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				singleRuleEngine.Evaluate(context.TODO(), input, ruleenginecore.EvaluateOptions().Complete())
+			}
+		})
+	}
+}
